afd40b55e02ab5479039f9e9ad071d2b239d07d0
learnforpractice <learnforpractice@gmail.com>
2021-08-24 13:33:11 +0800

    Update examples

---
+++ examples/hello/hello.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"chain"
+	"chain/logger"
+)
+
+//contract hello
+type MyContract struct {
+	Receiver      chain.Name
+	FirstReceiver chain.Name
+	Action        chain.Name
+}
+
+func NewContract(receiver, firstReceiver, action chain.Name) *MyContract {
+	return &MyContract{receiver, firstReceiver, action}
+}
+
+//action sayhello
+func (c *MyContract) SayHello(name string) {
+	logger.Println("Hello", name)
+}
