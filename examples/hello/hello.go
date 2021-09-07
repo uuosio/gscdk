67325b2b561c9efda0578e7094e9a15c196c0401
learnforpractice <learnforpractice@gmail.com>
2021-09-07 17:49:34 +0800

    Cleanup code

--- examples/hello/hello.go
+++ examples/hello/hello.go
@@ -17,7 +17,5 @@ func NewContract(receiver, firstReceiver, action chain.Name) *MyContract {
 
 //action sayhello
 func (c *MyContract) SayHello(name string) {
-	a := make([]string, 1)
-	chain.Check(a[0] == "", a[0])
 	chain.Println("Hello", name, a)
 }
