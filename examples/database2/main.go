ac67a08981d3d070e850cfcc3452f8ce8af8f320
learnforpractice <learnforpractice@gmail.com>
2021-09-08 14:23:07 +0800

    Add singleton example

---
+++ examples/database2/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/uuosio/chain"
+)
+
+//table mydata2 singleton
+type MySingleton struct {
+	a1 uint64
+}
+
+//contract mycontract
+type MyContract struct {
+	Receiver      chain.Name
+	FirstReceiver chain.Name
+	Action        chain.Name
+}
+
+func NewContract(receiver, firstReceiver, action chain.Name) *MyContract {
+	return &MyContract{receiver, firstReceiver, action}
+}
+
+//action sayhello
+func (t *MyContract) SayHello() {
+	code := t.Receiver
+	payer := t.Receiver
+	db := NewMySingletonDB(code, code)
+
+	data := db.Get()
+	if data != nil {
+		println("+++a1:", data.a1)
+		data.a1 += 1
+		db.Set(data, payer)
+	} else {
+		s := MySingleton{}
+		db.Set(&s, payer)
+	}
+}
