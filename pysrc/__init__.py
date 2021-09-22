b605534e51757f69223fe2d9a62a0d6024a097f6
learnforpractice <learnforpractice@gmail.com>
2021-09-22 15:59:31 +0800

    Updates for start binder

--- pysrc/__init__.py
+++ pysrc/__init__.py
@@ -1,28 +1,22 @@
 import os
 import sys
 import subprocess
-import shutil
+import shlex
 
 def run_tinygo():
     dir_name = os.path.dirname(os.path.realpath(__file__))
-    os.chdir(dir_name + "/tinygo/bin")
-
+    dir_name = os.path.join(dir_name, "tinygo")
+    os.environ['GOROOT'] = dir_name
+    os.chdir(dir_name)
     if len(sys.argv) <= 1:
-        cmd = ['tinygo']
-        subprocess.call(cmd)
+        cmd = [f'{dir_name}/bin/tinygo']
+        subprocess.call(cmd, cwd=dir_name)
     elif sys.argv[1] == "build":
-        cmd = 'tinygo build -x -gc=leaking -target eosio -wasm-abi=generic -scheduler=none -opt z -tags=math_big_pure_go'
-        cmd = shutil.split(cmd)
+        cmd = f'{dir_name}/bin/tinygo build -x -gc=leaking -target eosio -wasm-abi=generic -scheduler=none -opt z -tags=math_big_pure_go'
+        cmd = shlex.split(cmd)
         cmd.extend(sys.argv[2:])
-        subprocess.call(cmd)
+        subprocess.call(cmd, cwd=dir_name)
     else:
-        cmd = ['tinygo']
+        cmd = [f'{dir_name}/bin/tinygo']
         cmd.extend(sys.argv[1:])
-        subprocess.call(cmd)
-# if [ $1 == "build" ]; then
-# tinygo build -x -gc=leaking -target eosio -wasm-abi=generic -scheduler=none -opt z -tags=math_big_pure_go ${@:2}
-# else
-# tinygo $@
-# fi
-
-
+        subprocess.call(cmd, cwd=dir_name)
