baf5afd4caf3e65a494f651ec0333cdaa044d039
learnforpractice <learnforpractice@gmail.com>
2021-10-08 17:55:27 +0800

    remove os.chdir

--- pysrc/__init__.py
+++ pysrc/__init__.py
@@ -7,7 +7,6 @@ def run_tinygo():
     dir_name = os.path.dirname(os.path.realpath(__file__))
     dir_name = os.path.join(dir_name, "tinygo")
     os.environ['GOROOT'] = dir_name
-    os.chdir(dir_name)
     if len(sys.argv) <= 1:
         cmd = [f'{dir_name}/bin/tinygo']
         subprocess.call(cmd)
