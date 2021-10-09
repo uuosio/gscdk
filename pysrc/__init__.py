d8b9a2e9d526dc5f1378881c34b2288dd6f5bf70
learnforpractice <learnforpractice@gmail.com>
2021-10-09 19:55:01 +0800

    return call result

--- pysrc/__init__.py
+++ pysrc/__init__.py
@@ -8,15 +8,15 @@ def run_tinygo():
     dir_name = os.path.join(dir_name, "tinygo")
     tinygo = os.path.join(dir_name, 'bin/tinygo')
     if len(sys.argv) <= 1:
-        subprocess.call(tinygo)
+        return subprocess.call(tinygo)
     elif sys.argv[1] == "build":
         cmd = [tinygo]
         args = 'build -x -gc=leaking -target eosio -wasm-abi=generic -scheduler=none -opt z -tags=math_big_pure_go'
         args = shlex.split(args)
         cmd.extend(args)
         cmd.extend(sys.argv[2:])
-        subprocess.call(cmd)
+        return subprocess.call(cmd)
     else:
         cmd = sys.argv[:]
         cmd[0] = tinygo
-        subprocess.call(cmd)
+        return subprocess.call(cmd)
