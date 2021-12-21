c31b48ed71d51b6673b2533abb3c48208c136f1e
learnforpractice <learnforpractice@gmail.com>
2021-12-21 11:16:55 +0800

    Add verify signature example

---
+++ examples/verifysignature/go.mod
@@ -0,0 +1,5 @@
+module test
+
+go 1.16
+
+require github.com/uuosio/chain v0.1.2
---
+++ examples/verifysignature/verifier.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/uuosio/chain"
+)
+
+//contract verifier
+type MyContract struct {
+	Receiver      chain.Name
+	FirstReceiver chain.Name
+	Action        chain.Name
+}
+
+func NewContract(receiver, firstReceiver, action chain.Name) *MyContract {
+	return &MyContract{receiver, firstReceiver, action}
+}
+
+//action verify
+func (c *MyContract) Verify(data string, public_key *chain.PublicKey, signature *chain.Signature) {
+	digest := chain.Sha256([]byte(data))
+	recovered_pub := chain.RecoverKey(digest, signature)
+	chain.Assert(*recovered_pub == *public_key, "invalid signature")
+	chain.Println("verify success")
+}
