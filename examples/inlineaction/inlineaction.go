25b7e1c39d7ed7895512fc4f885e046613243c57
learnforpractice <learnforpractice@gmail.com>
2022-01-21 20:03:17 +0800

    Update inline action example

--- examples/inlineaction/inlineaction.go
+++ examples/inlineaction/inlineaction.go
@@ -15,18 +15,17 @@ type Transfer struct {
 func main() {
 	_, _, action := chain.GetApplyArgs()
 	if action == chain.NewName("sayhello") {
-		a := chain.NewAction(chain.PermissionLevel{chain.NewName("helloworld11"), chain.ActiveName}, chain.NewName("eosio.token"), chain.NewName("transfer"))
-
-		t := Transfer{}
-		t.From = chain.NewName("helloworld11")
-		t.To = chain.NewName("eosio")
-
-		// Send 1.0 EOS
-		t.Quantity.Amount = 10000
-		t.Quantity.Symbol = chain.NewSymbol("EOS", 4)
-
-		a.Data = t.Pack()
-		a.Send()
+		t := Transfer{
+			From:     chain.NewName("helloworld11"),
+			To:       chain.NewName("eosio"),
+			Quantity: *chain.NewAsset(10000, chain.NewSymbol("EOS", 4)),
+			Memo:     "hello",
+		}
+		chain.NewAction(
+			&chain.PermissionLevel{chain.NewName("helloworld11"), chain.ActiveName},
+			chain.NewName("eosio.token"),
+			chain.NewName("transfer"),
+			&t,
+		).Send()
 	}
-
 }
