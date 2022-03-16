2699905d17030c3cb2fae8b8766ef7a47aec3501
learnforpractice <learnforpractice@gmail.com>
2022-03-16 16:55:00 +0800

    Update callcpp example

--- examples/callcpp/hello.cpp
+++ examples/callcpp/hello.cpp
@@ -1,4 +1,28 @@
 #include <eosio/eosio.hpp>
+#include <eosio/multi_index.hpp>
+
+using namespace eosio;
+
+struct record {
+   uint64_t    primary;
+   uint128_t   secondary;
+   uint64_t    data;
+   uint64_t primary_key() const { return primary; }
+   uint128_t get_secondary() const { return secondary; }
+   EOSLIB_SERIALIZE( record, (primary)(secondary)(data))
+};
+
 extern "C" void say_hello() {
     eosio::print("++++++++hello,world\n");
 }
+
+
+extern "C" bool find_db(uint64_t code, uint64_t scope, uint64_t key) {
+    multi_index<"mytable4"_n,
+                record,
+                indexed_by< "bysecondary"_n,
+                const_mem_fun<record, uint128_t, &record::get_secondary> > > mytable(name{code}, scope);
+    auto it = mytable.find(key);
+    return it != mytable.end();
+}
+
--- examples/callcpp/main.go
+++ examples/callcpp/main.go
@@ -1,10 +1,17 @@
 package main
 
 /*
+#include <stdbool.h>
+#include <stdint.h>
+
 void say_hello();
+bool find_db(uint64_t code, uint64_t scope, uint64_t key);
 */
 import "C"
+import "github.com/uuosio/chain"
 
 func main() {
+	receiver, code, _ := chain.GetApplyArgs()
+	C.find_db(receiver.N, code.N, 1)
 	C.say_hello()
 }
