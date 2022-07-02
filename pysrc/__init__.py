ad65c9479006f851de2fc6443c0c5a2faa08dac4
learnforpractice <learnforpractice@gmail.com>
2022-07-02 21:45:20 +0800

    bump version to v0.6.0

--- pysrc/__init__.py
+++ pysrc/__init__.py
@@ -4,7 +4,7 @@ import subprocess
 import shlex
 import platform
 
-__version__ = "0.5.1"
+__version__ = "0.6.0"
 
 def run_tinygo():
     dir_name = os.path.dirname(os.path.realpath(__file__))
--- tag.sh
+++ tag.sh
@@ -1,4 +1,4 @@
-VERSION=v0.5.1
+VERSION=v0.6.0
 TARGET=origin
 git push $TARGET :refs/tags/$VERSION
 git tag -d $VERSION
